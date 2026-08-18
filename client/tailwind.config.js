281bc2d9d911f233a0ec1b8d817af0ed6d87880c
ryan sheng <ryansheng@proton.me>
2026-08-18 01:36:11 -0700

    Tailwind + Material setup

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
